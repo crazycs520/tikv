b62fa22589574e69d494685b46503b2b33854a31
crazycs <crazycs520@gmail.com>
2021-08-04 18:50:38 +0800

    add record for point get
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -87,6 +87,7 @@ use kvproto::pdpb::QueryKind;
 use raftstore::store::util::build_key_range;
 use rand::prelude::*;
 use resource_metering::{cpu::FutureExt, ResourceMeteringTag};
+use resource_metering::row::recorder::add_thread_scan_row;
 use std::{
     borrow::Cow,
     iter,
@@ -381,6 +382,7 @@ impl<E: Engine, L: LockManager> Storage<E, L> {
                             r
                         });
 
+                    add_thread_scan_row(statistics.write.processed_keys as u64);
                     metrics::tls_collect_scan_details(CMD, &statistics);
                     metrics::tls_collect_read_flow(ctx.get_region_id(), &statistics);
                     SCHED_PROCESSING_READ_HISTOGRAM_STATIC
@@ -626,6 +628,7 @@ impl<E: Engine, L: LockManager> Storage<E, L> {
                             kv_pairs
                         });
 
+                    add_thread_scan_row(statistics.write.processed_keys as u64);
                     metrics::tls_collect_scan_details(CMD, &statistics);
                     metrics::tls_collect_read_flow(ctx.get_region_id(), &statistics);
                     SCHED_PROCESSING_READ_HISTOGRAM_STATIC
@@ -763,6 +766,7 @@ impl<E: Engine, L: LockManager> Storage<E, L> {
                     let res = scanner.scan(limit, sample_step);
 
                     let statistics = scanner.take_statistics();
+                    add_thread_scan_row(statistics.write.processed_keys as u64);
                     metrics::tls_collect_scan_details(CMD, &statistics);
                     metrics::tls_collect_read_flow(ctx.get_region_id(), &statistics);
                     SCHED_PROCESSING_READ_HISTOGRAM_STATIC
