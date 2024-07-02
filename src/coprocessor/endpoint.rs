dd3bbfbd5c47a1e83416ea980d9aa71472bc2f4a
crazycs520 <crazycs520@gmail.com>
2024-07-02 15:00:16 +0800

    fix
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- src/coprocessor/endpoint.rs
+++ src/coprocessor/endpoint.rs
@@ -904,16 +904,16 @@ impl<E: Engine> Endpoint<E> {
         let req_is_cache_enabled = req.get_is_cache_enabled();
         let mut input = CodedInputStream::from_bytes(data);
         box_try!(dag.merge_from(&mut input));
+        let mut offset = Vec::new();
         let extra_executor = dag.take_extra_executors();
-        dag.set_executors(extra_executor);
-        if dag.get_extra_executors().len() > 0 {
-            let mut offset = Vec::new();
-            let schema_len = dag.get_extra_executors()[0].get_tbl_scan().get_columns().len();
+        if extra_executor.len() > 0 {
+            let schema_len = extra_executor[0].get_tbl_scan().get_columns().len();
             for i in 0.. schema_len {
                 offset.push(i as u32);
             }
-            dag.set_output_offsets(offset);
         }
+        dag.set_output_offsets(offset);
+        dag.set_executors(extra_executor);
         let extra_executor = dag.get_executors();
         if extra_executor.len() > 0 {
             info!("index lookup build extra executor";
