992c4dcf4da52c3c909f3847e4ec7580281bfe33
crazycs520 <crazycs520@gmail.com>
2025-01-15 20:59:18 +0800

    remove lease check
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- components/raftstore/src/store/worker/read.rs
+++ components/raftstore/src/store/worker/read.rs
@@ -371,28 +371,29 @@ where
                 let end = start.clone();
                 for (_end_key, id) in meta.region_ranges.range((start, end)) {
                     if let Some(reader) = meta.readers.get(id) {
-                        let in_lease = reader.is_in_leader_lease(monotonic_raw_now());
-                        if in_lease && util::check_key_in_region(key, &reader.region).is_ok() {
-                            //     info!("locate key exist and valid";
-                            // "key" => ?key,
-                            // "end_key" => ?_end_key,
-                            // "region_start_key" => ?reader.region.start_key,
-                            // "region_end_key" => ?reader.region.end_key,
-                            // "region_id" => reader.region.id,
-                            // "term" => reader.term);
-                            return Some((reader.region.clone(), reader.peer_id, reader.term));
-                        } else {
-                            info!("locate key exist, but not valid";
-                            "key" => ?key,
-                            "end_key" => ?_end_key,
-                            "region_start_key" => ?reader.region.start_key,
-                            "region_end_key" => ?reader.region.end_key,
-                            "in_lease" => in_lease,
-                            "contain" => util::check_key_in_region(key,
-                            &reader.region).is_ok(),
-                            "region_id" => reader.region.id,
-                            "term" => reader.term);
-                        }
+                        return Some((reader.region.clone(), reader.peer_id, reader.term));
+                        // if util::check_key_in_region(key,
+                        // &reader.region).is_ok() {
+                        //     info!("locate key exist and valid";
+                        // "key" => ?key,
+                        // "end_key" => ?_end_key,
+                        // "region_start_key" => ?reader.region.start_key,
+                        // "region_end_key" => ?reader.region.end_key,
+                        // "region_id" => reader.region.id,
+                        // "term" => reader.term);
+                        // return Some((reader.region.clone(), reader.peer_id,
+                        // reader.term)); } else {
+                        // info!("locate key exist, but not valid";
+                        // "key" => ?key,
+                        // "end_key" => ?_end_key,
+                        // "region_start_key" => ?reader.region.start_key,
+                        // "region_end_key" => ?reader.region.end_key,
+                        // "in_lease" => in_lease,
+                        // "contain" => util::check_key_in_region(key,
+                        // &reader.region).is_ok(),
+                        // "region_id" => reader.region.id,
+                        // "term" => reader.term);
+                        // }
                     } else {
                         info!("locate key not exist"; "key" => ?key);
                     }
